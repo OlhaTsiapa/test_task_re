a625b2f31f221550b0452c6a1c16f893165f3fb9
Olha Tsiapa <o.tsiapa@quantumobile.com>
2019-01-11 18:19:34 +0200

    'update task'

--- task_re.py
+++ task_re.py
@@ -11,22 +11,54 @@
 # Then, the output of the program should be:
 # Company: google, user: john
 
-import argparse
+# import argparse
 import re
+import argparse
 
-email = input('Input email:')
-parsing_email = re.compile(r'(?P<user>[a-z0-9]+)@(?P<company>(?:[a-z0-9]+))')
-result = parsing_email.search(email)
-user = result.group("user")
-company = result.group("company")
-
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(description='Test.')
+parser.add_argument('--email', type=str)
 parser.add_argument('-c', '--clear', action="store_true")
-args = parser.parse_args()
 
-if args.clear:
-    name_user = re.sub(r'\d+', '', user)
-    print("Company: %s, User: %s" % (company, name_user))
-else:
+args = parser.parse_args()
+email = args.email
+
+
+# if args.clear:
+#     name_user = re.sub(r'\d+', '', user)
+#     print("Company: %s, User: %s" % (company, name_user))
+
+if email:
+    parsing_email = re.compile(r'(?P<user>[a-z0-9]+)@(?P<company>(?:[a-z0-9]+))')
+    result = parsing_email.search(email)
+    user = result.group("user")
+    company = result.group("company")
     print("Company: %s, User: %s" % (company, user))
 
+
+# email = input('Input email:')
+# parsing_email = re.compile(r'(?P<user>[a-z0-9]+)@(?P<company>(?:[a-z0-9]+))')
+# result = parsing_email.search(email)
+# user = result.group("user")
+# company = result.group("company")
+#
+# parser = argparse.ArgumentParser()
+# parser.add_argument('-c', '--clear', action="store_true")
+# parser.add_argument('email', action="store", dest="", type=str)
+# args = parser.parse_args()
+#
+# # if args.clear:
+#     name_user = re.sub(r'\d+', '', user)
+#     print("Company: %s, User: %s" % (company, name_user))
+# # else:
+#     print("Company: %s, User: %s" % (company, user))
+
+
+import argparse
+#
+# parser = argparse.ArgumentParser(description='Short sample app')
+#
+# parser.add_argument('-a', action="store_true", default=False)
+# parser.add_argument('-b', action="store", dest="b")
+# parser.add_argument('-c', action="store", dest="c", type=int)
+# #
+# print(parser.parse_args(['-a', '-bval', '-c', '3']))
